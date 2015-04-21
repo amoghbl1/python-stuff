e03ad287d4c71d0b00de85c2402251cef137f713
Amogh Pradeep <amoghbl1@gmail.com>
2015-04-22 03:42:28 +0530

    makePdf script added

---
+++ makePdf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import pyPdf, sys, os
+from PIL import Image
+from pyPdf import PdfFileWriter, PdfFileReader
+if len(sys.argv) == 1:
+    print "USAGE: makePdf folder_name"
+    sys.exit(2)
+path = sys.argv[1]
+output = PdfFileWriter()
+images = os.listdir(path)
+for i in images: # Create temporary images and save them as pdfs
+    imagePath = path+i
+    print imagePath
+    tempImagePath = imagePath+".temp"
+    img = Image.open(imagePath)
+    img.save(tempImagePath, "PDF", resolution=100.0)
+    inputConvertedImage = PdfFileReader(file(tempImagePath, "rb"))
+    output.addPage(inputConvertedImage.getPage(0))
+outputStream = file("output.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
+for i in images: #Delete all the temporary pdf files created
+    os.remove(path+i+".temp")
